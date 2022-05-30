1e8cb480dd81e6067d3f1ad17b264061b7ef871f
Nata1802 <nata12102014@yandex.ru>
2022-05-30 21:14:51 +0300

    leason1 3005

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import NavPan from "./Components/NavPan/NavPan";
 import Tecnologes from "./Components/Tecnologes/Tecnologes";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+
         <NavPan />
+<BrowserRouter>
+    <Routes>
+            <Route path="/main/*" element={<Tecnologes />} />
+    </Routes>
+</BrowserRouter>
         <Tecnologes />
      </div>
   );
--- src/Components/Nav_box/Nav_box.js
+++ src/Components/Nav_box/Nav_box.js
@@ -1,5 +1,5 @@
 import './Nav_box.css';
-import {NavLink} from 'react'
+
 
 
 function NavBox(){
